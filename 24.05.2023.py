c0cca7f7ede013703786083a195e5d002728b4cb
vitindotrembala <133054224+vitindotrembala@users.noreply.github.com>
2023-05-24 18:02:34 -0300

    Add files via upload

--- 24.05.2023.py
+++ 24.05.2023.py
@@ -1,11 +1,9 @@
-nome=input("nome?")
-while nome!="":
-    nota1= float (input("nota1?"))
-    nota2= float (input("nota2?"))
-    media= 0.4* nota1 + 0.6* nota2
-    nome= nome.capitalize()
-print(f"o nome eh {n:>10s} e a média eh {media: 5.1f}")
-nome= input("nome")
+fato=int(input("digite um numero"))
+fatorial= 1
 
+while fato>0:
+    fatorial = fato*fatorial
+    fato= fato-1
+print(fatorial)
 
 
