b1f65d5063c000a113a72410f33e11d380428cf4
vitindotrembala <133054224+vitindotrembala@users.noreply.github.com>
2023-05-24 17:43:17 -0300

    Add files via upload

---
+++ 24.05.2023.py
@@ -0,0 +1,11 @@
+nome=input("nome?")
+while nome!="":
+    nota1= float (input("nota1?"))
+    nota2= float (input("nota2?"))
+    media= 0.4* nota1 + 0.6* nota2
+    nome= nome.capitalize()
+print(f"o nome eh {n:>10s} e a média eh {media: 5.1f}")
+nome= input("nome")
+
+
+
